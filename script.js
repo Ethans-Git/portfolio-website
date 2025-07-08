449feed0c60bb3bea6a324cc79942a9813f70809
Ethan Quach <enq@bu.edu>
2025-07-08 15:04:42 -0400

    Added new assets and animations to website

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log("hello world")
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('.links a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+        
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active')
+                document.querySelector('.links a[href*=' + id + ']').classList.add('active');
+                console.log(document.querySelector('.links a[href*=' + id + ']').classList);
+            })
+        }
+    })
+}
